b3669fed2bbfb32088d59856de4db09f942ac047
Firefly4266 <gaines.johnp@gmail.com>
2017-11-22 13:55:01 -0800

    added and completed first series of testing.

---
+++ TestExercise/fileProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercise
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(fileName);
+            }
+            else
+            {
+                return File.Exists(fileName);
+            }
+        }
+    }
+}
---
+++ TestExerciseTest/File ProcessTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestExercise;
+
+namespace TestExerciseTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            //Arrange
+            FileProcess fileProcess = new FileProcess();
+            //Act
+            bool fromCall;
+            fromCall = fileProcess.FileExists(@"c:\\Windows\Regedit.exe");
+            //Assert
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            //Arrange
+            FileProcess fileProcess = new FileProcess();
+            //Act
+            bool fromCall;
+            fromCall = fileProcess.FileExists(@"c:\\badFile.bad");
+            //Assert
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_throwsArgumentException()
+        {
+            //Arrange
+            FileProcess fileProcess = new FileProcess();
+            //Act
+            try
+            {
+                fileProcess.FileExists(@"c:\\badFile.bad");
+            }
+            catch (ArgumentNullException)
+            {
+
+                throw new ArgumentNullException(@"c:\\badFile.bad");
+            }
+        }
+    }
+}
